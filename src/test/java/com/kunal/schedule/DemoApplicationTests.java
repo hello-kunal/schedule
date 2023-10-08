894441b4e9fc5777a37d7b605ab91d2c794d7290
hello-kunal <136134149+hello-kunal@users.noreply.github.com>
2023-10-08 13:26:00 +1300

    Update DemoApplicationTests.java

--- src/test/java/com/kunal/schedule/DemoApplicationTests.java
+++ src/test/java/com/kunal/schedule/DemoApplicationTests.java
@@ -3,11 +3,10 @@ package com.kunal.schedule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplicationTests.class)
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
